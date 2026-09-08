1f8b15519cfaf977f43299cb65f99f9fdb378947
jonathanwhiddon-vpll <jonathan.whiddon@gmail.com>
2026-09-08 06:49:00 -0700

    Update app-v2.js

--- app-v2.js
+++ app-v2.js
@@ -2129,7 +2129,7 @@ function renderTicker(forceRestart = false) {
   if (!tickerData || tickerData.length === 0) {
     html = `
       <div class="ticker-item">
-        🍂 <span class="no-scores">Schedules will be released Sep 9. Click Fall Ball Practices to see your practice slots.</span>
+        🍂 <span class="no-scores">Schedules will be released this week. Click Fall Ball Practices to see your practice slots.</span>
     </div>
     `;
   } else {
